287b9ebf33246ddf67e30f6a0af81c05dcfd1975
Natalia Kostka <natalia7kostka@gmail.com>
2023-01-30 18:35:29 -0700

    updated dream model

--- server/models/dreamModel.js
+++ server/models/dreamModel.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
 const dreamSchema = mongoose.Schema({
-    entry: String,
-    hoursOfSleep: Number,
-    // createdAt: {
-    //     type: Date,
-    //     default: new Date()
-    // }
-});
+    entry: {
+        type: String,
+        required: true
+    },
+    hoursOfSleep: {
+        type: Number,
+        required: true
+    }
+}, {timestamps: true});
 
 const Dream = mongoose.model('Dream', dreamSchema);
 
