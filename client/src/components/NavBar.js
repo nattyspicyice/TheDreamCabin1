abf3071e61d6870f6003412ce1f770ed22d9b5ae
Natalia Kostka <natalia7kostka@gmail.com>
2023-02-01 21:33:34 -0700

    added url to nav link path

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -13,7 +13,7 @@ const NavBar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
         </Navbar.Collapse>
         <Nav>
-            <Nav.Link href="/login">Logout</Nav.Link>
+            <Nav.Link href="https://the-dream-cabin-backend-2.onrender.com/login">Logout</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
